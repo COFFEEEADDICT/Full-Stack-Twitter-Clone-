b54798b16ce15df1d9ee72137dae52cda8d88ed8
bryn <thenazboz@gmail.com>
2020-07-07 15:07:50 +0100

    front + back end working state

--- client/client.js
+++ client/client.js
@@ -1,11 +1,15 @@
 //select the only form
 const form = document.querySelector("form");
 
+const mewsElement = document.querySelector(".mews");
+
 const API_URL = "http://localhost:5000/mews";
 
 //select the loader
 const loadingElement = document.querySelector(".loading");
-loadingElement.style.display = "none";
+loadingElement.style.display = "";
+
+listAllMews();
 
 // submit button (send meow)
 form.addEventListener("submit", (event) => {
@@ -31,5 +35,38 @@ form.addEventListener("submit", (event) => {
 		},
 	})
 		.then((res) => res.json())
-		.then((createdMew) => console.log(createdMew));
+		.then((createdMew) => {
+			console.log(createdMew);
+			form.reset();
+			form.style.display = "";
+			listAllMews();
+		});
 });
+
+//GET all mews/format
+function listAllMews() {
+	mewsElement.innerHTML = "";
+	fetch(API_URL)
+		.then((res) => res.json())
+		.then((mews) => {
+			mews.reverse();
+			mews.forEach((mew) => {
+				const div = document.createElement("div");
+
+				const header = document.createElement("h3");
+				header.textContent = mew.name;
+
+				const contents = document.createElement("p");
+				contents.textContent = mew.content;
+
+				const date = document.createElement("small");
+				date.textContent = new Date(mew.created);
+
+				div.appendChild(header);
+				div.appendChild(contents);
+				div.appendChild(date);
+				mewsElement.appendChild(div);
+			});
+			loadingElement.style.display = "none";
+		});
+}
